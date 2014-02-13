e86e7dc5a917d7804483f307933bcdb427d9e66b
Settapak Yeerunsiri <settapak@gmail.com>
2014-02-13 13:59:02 +0700

    Create Unity project
    - Create Unity project
    - Create Gameplay scene

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		Debug.Log("Force!!!");
+		Vector2 force = new Vector2(0, 100);
+		Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+		//rigid.mass * rigid.acc
+		rigid.AddForce(force);
+	}
+}
